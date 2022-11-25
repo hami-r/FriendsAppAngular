91ff8b85fff7da328bd169743baacdfc6dc6d3b5
hami-r <mynameissaitama54@gmail.com>
2022-11-25 17:48:20 +0530

    view component created and data binding done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewFriendsComponent } from './view-friends/view-friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewFriendsComponent
   ],
   imports: [
     BrowserModule,
